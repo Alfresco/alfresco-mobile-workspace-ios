1fa08c56b06da55b86c6235bb82a1a96c7b9b2aa
Florin Baincescu <florin.baincescu@ness.com>
2020-05-25 17:16:29 +0300

    MOBILEAPPS-310 Implement UI using design specs

--- ContentApp/Screens/Login/BasicAuthViewController.swift
+++ ContentApp/Screens/Login/BasicAuthViewController.swift
@@ -1,9 +1,19 @@
 //
-//  BasicAuthViewController.swift
-//  ContentApp
+// Copyright (C) 2005-2020 Alfresco Software Limited.
 //
-//  Created by Florin Baincescu on 25/05/2020.
-//  Copyright © 2020 Florin Baincescu. All rights reserved.
+// This file is part of the Alfresco Content Mobile iOS App.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 import UIKit
@@ -20,8 +30,14 @@ class BasicAuthViewController: UIViewController {
 
     @IBOutlet weak var signInButton: UIButton!
 
-    var authParameters = AuthSettingsParameters.parameters()
+    var model = BasicAuthViewModel()
     var keyboardHandling = KeyboardHandling()
+    var enableSignInButton: Bool = false {
+        didSet {
+            signInButton.isEnabled = enableSignInButton
+            signInButton.tintColor = signInButton.currentTitleColor
+        }
+    }
 
     // MARK: - View Life Cycle
 
@@ -30,13 +46,17 @@ class BasicAuthViewController: UIViewController {
 
         addLocalization()
         updateLayout()
-        shouldEnableSignInButton()
+        enableSignInButton = false
     }
 
     // MARK: - IBActions
 
     @IBAction func signInButtonTapped(_ sender: UIButton) {
         self.view.endEditing(true)
+        guard let username = usernameTextField.text, let password = passwordTextField.text else {
+            return
+        }
+        model.authenticate(username: username, password: password)
     }
 
     @IBAction func viewTapped(_ sender: UITapGestureRecognizer) {
@@ -66,17 +86,12 @@ class BasicAuthViewController: UIViewController {
 
     func addLocalization() {
         self.title = ""
-        infoLabel.text = String(format: LocalizationConstants.Labels.infoConnectTo, authParameters.hostname)
+        infoLabel.text = String(format: LocalizationConstants.Labels.infoConnectTo, model.authParameters.hostname)
         usernameTextField.placeholder = LocalizationConstants.TextFieldPlaceholders.username
         passwordTextField.placeholder = LocalizationConstants.TextFieldPlaceholders.password
         signInButton.setTitle(LocalizationConstants.Buttons.signin, for: .normal)
         signInButton.setTitle(LocalizationConstants.Buttons.signin, for: .disabled)
-    }
-
-    func shouldEnableSignInButton() {
-        let enable = (usernameTextField.text != "" && passwordTextField.text != "")
-        signInButton.isEnabled = enable
-        signInButton.tintColor = signInButton.currentTitleColor
+        copyrightLabel.text = String(format: LocalizationConstants.copyright, Calendar.current.component(.year, from: Date()))
     }
 
     // MARK: - Navigation
@@ -94,12 +109,17 @@ extension BasicAuthViewController: UITextFieldDelegate {
         let heightTextFieldOpened = textFieldRect.size.height
         keyboardHandling.add(positionObjectInSuperview: textFieldRect.origin.y + heightTextFieldOpened,
                              in: view)
-
         return true
     }
 
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        shouldEnableSignInButton()
+        switch textField {
+        case usernameTextField:
+            enableSignInButton = (textField.updatedText(for: range, replacementString: string) != "" && passwordTextField.text != "")
+        case passwordTextField:
+            enableSignInButton = (textField.updatedText(for: range, replacementString: string) != "" && usernameTextField.text != "")
+        default: break
+        }
         return true
     }
 
@@ -107,21 +127,20 @@ extension BasicAuthViewController: UITextFieldDelegate {
         switch textField {
         case usernameTextField:
             passwordTextField.becomeFirstResponder()
-        default:
-            textField.resignFirstResponder()
+        case passwordTextField:
+            passwordTextField.resignFirstResponder()
+            signInButtonTapped(signInButton)
+        default: break
         }
         return true
     }
 
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
         textField.rightView?.tintColor = #colorLiteral(red: 0.7254338861, green: 0.7255221009, blue: 0.7254036665, alpha: 1)
-        shouldEnableSignInButton()
         return true
     }
 
     func textFieldDidEndEditing(_ textField: UITextField) {
-        if textField == passwordTextField && signInButton.isEnabled == true {
-            signInButtonTapped(signInButton)
-        }
+        enableSignInButton = (usernameTextField.text != "" && passwordTextField.text != "")
     }
 }
