28b91d26606fc927dfdadcc72d23f182fb2351d3
Florin Baincescu <florin.baincescu@ness.com>
2020-05-26 19:10:41 +0300

    MOBILEAPPS-324 Implement UI using design specs

--- ContentApp/Screens/Login/AimsViewController.swift
+++ ContentApp/Screens/Login/AimsViewController.swift
@@ -18,15 +18,23 @@
 
 import UIKit
 
+import MaterialComponents.MaterialButtons
+import MaterialComponents.MaterialButtons_Theming
+import MaterialComponents.MaterialTextControls_FilledTextFields
+import MaterialComponents.MaterialTextControls_FilledTextFieldsTheming
+
 class AimsViewController: UIViewController {
 
     @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var productLabel: UILabel!
     @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var hostnameLabel: UILabel!
     @IBOutlet weak var allowLabel: UILabel!
     @IBOutlet weak var copyrightLabel: UILabel!
 
-    @IBOutlet weak var repositoryTextField: UITextField!
-    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var repositoryTextField: MDCFilledTextField!
+    @IBOutlet weak var signInButton: MDCButton!
+    @IBOutlet weak var needHelpButton: MDCButton!
 
     var model = AimsViewModel()
     var keyboardHandling = KeyboardHandling()
@@ -43,6 +51,7 @@ class AimsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         addLocalization()
+        addMaterialComponentsTheme()
         enableSignInButton = (repositoryTextField.text != "")
     }
 
@@ -56,6 +65,9 @@ class AimsViewController: UIViewController {
         model.authenticate(repository: repositoryURL, in: self)
     }
 
+    @IBAction func needHelpButtonTapped(_ sender: Any) {
+    }
+
     @IBAction func viewTapped(_ sender: UITapGestureRecognizer) {
         view.endEditing(true)
     }
@@ -64,12 +76,42 @@ class AimsViewController: UIViewController {
 
     func addLocalization() {
         self.title = ""
-        infoLabel.text = String(format: LocalizationConstants.Labels.infoConnectTo, model.authParameters.hostname)
+        productLabel.text = LocalizationConstants.productName
+        infoLabel.text = LocalizationConstants.Labels.infoConnectTo
+        hostnameLabel.text = model.authParameters.hostname
         allowLabel.text = LocalizationConstants.Labels.allowSSO
-        repositoryTextField.placeholder = LocalizationConstants.TextFieldPlaceholders.repository
+        repositoryTextField.label.text = LocalizationConstants.TextFieldPlaceholders.repository
         signInButton.setTitle(LocalizationConstants.Buttons.signInWithSSO, for: .normal)
         signInButton.setTitle(LocalizationConstants.Buttons.signInWithSSO, for: .disabled)
+        needHelpButton.setTitle(LocalizationConstants.Buttons.needHelp, for: .normal)
         copyrightLabel.text = String(format: LocalizationConstants.copyright, Calendar.current.component(.year, from: Date()))
+    }
+
+    func addMaterialComponentsTheme() {
+        let theme = MaterialDesignThemingService()
+        theme.activeTheme = DefaultTheme()
+
+        signInButton.applyContainedTheme(withScheme: theme.containerScheming(for: .loginButton))
+        needHelpButton.applyTextTheme(withScheme: theme.containerScheming(for: .loginNeedHelpButton))
+
+        repositoryTextField.applyTheme(withScheme: theme.containerScheming(for: .loginTextField))
+        repositoryTextField.setFilledBackgroundColor(.clear, for: .normal)
+        repositoryTextField.setFilledBackgroundColor(.clear, for: .editing)
+
+        productLabel.textColor = theme.activeTheme?.productLabelColor
+        productLabel.font = theme.activeTheme?.productLabelFont
+
+        infoLabel.textColor = theme.activeTheme?.loginInfoLabelColor
+        infoLabel.font = theme.activeTheme?.loginInfoLabelFont
+
+        hostnameLabel.textColor = theme.activeTheme?.loginInfoLabelColor
+        hostnameLabel.font = theme.activeTheme?.loginInfoHostnameLabelFont
+
+        allowLabel.textColor = theme.activeTheme?.loginFieldLabelColor
+        allowLabel.font = theme.activeTheme?.loginInfoHostnameLabelFont
+
+        copyrightLabel.textColor = theme.activeTheme?.loginCopyrightLabelColor
+        copyrightLabel.font = theme.activeTheme?.loginCopyrightLabelFont
     }
 
     // MARK: - Navigation
