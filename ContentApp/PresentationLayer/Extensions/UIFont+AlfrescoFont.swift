42718aed952a1291f1e6354120d24fd7f623dd86
Florin Baincescu <florin.baincescu@ness.com>
2020-06-02 10:50:22 +0300

    Added extension for UIFont system

---
+++ ContentApp/PresentationLayer/Extensions/UIFont+AlfrescoFont.swift
@@ -0,0 +1,16 @@
+//
+//  UIFont+Helpers.swift
+//  ContentApp
+//
+//  Created by Florin Baincescu on 29/05/2020.
+//  Copyright © 2020 Florin Baincescu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    class func alfrescoRegularFont(ofSize fontSize: CGFloat) -> UIFont {
+        return UIFont.systemFont(ofSize: fontSize)
+    }
+}
