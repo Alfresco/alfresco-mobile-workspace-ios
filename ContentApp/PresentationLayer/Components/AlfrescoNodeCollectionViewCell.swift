ae21ce43e199fec4c5c24cecd44f8d852327ee36
Florin Baincescu <florin.baincescu@ness.com>
2020-07-07 15:34:52 +0300

    MOBILEAPPS-419 [iOS] Add basic search support

---
+++ ContentApp/PresentationLayer/Components/AlfrescoNodeCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  AlfrescoNodeCollectionViewCell.swift
+//  ContentApp
+//
+//  Created by Florin Baincescu on 06/07/2020.
+//  Copyright © 2020 Florin Baincescu. All rights reserved.
+//
+
+import UIKit
+
+class AlfrescoNodeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ ContentApp/PresentationLayer/Recent/Screens/ResultViewController.swift
@@ -0,0 +1,72 @@
+//
+// Copyright (C) 2005-2020 Alfresco Software Limited.
+//
+// This file is part of the Alfresco Content Mobile iOS App.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    var resultsNodes: [AlfrescoNode] = [] {
+        didSet {
+            collectionView.reloadData()
+        }
+    }
+
+    // MARK: - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        resultsNodes = []
+        registerAlfrescoNodeCell()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+    // MARK: - Helpers
+
+    func registerAlfrescoNodeCell() {
+        let identifier = String(describing: AlfrescoNodeCollectionViewCell.self)
+        collectionView?.register(UINib(nibName: identifier, bundle: nil), forCellWithReuseIdentifier: identifier)
+    }
+}
+
+// MARK: - UICollectionView DataSource & Delegate
+
+extension ResultViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return resultsNodes.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let node = resultsNodes[indexPath.row]
+        let identifier = String(describing: AlfrescoNodeCollectionViewCell.self)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as? AlfrescoNodeCollectionViewCell
+        cell?.node = node
+        return cell ?? UICollectionViewCell()
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.view.bounds.width, height: 64.0)
+    }
+}
+
+// MARK: - Storyboard Instantiable
+
+extension ResultViewController: StoryboardInstantiable { }
