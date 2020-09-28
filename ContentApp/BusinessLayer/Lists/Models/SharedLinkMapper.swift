6614f0896e74dd971188534d24799422dcefeb39
Florin Baincescu <florin.baincescu@ness.com>
2020-09-28 12:28:32 +0300

    MOBILEAPPS-531 [iOS] Cannot preview files from Shared screen

--- ContentApp/BusinessLayer/Lists/Models/SharedLinkMapper.swift
+++ ContentApp/BusinessLayer/Lists/Models/SharedLinkMapper.swift
@@ -29,6 +29,6 @@ struct SharedLinkMapper {
     }
 
     private static func create(from node: SharedLink) -> ListNode {
-        return ListNode(guid: node._id ?? "", mimeType: node.content?.mimeType, title: node.name ?? "", path: "", modifiedAt: node.modifiedAt, kind: .file)
+        return ListNode(guid: node.nodeId ?? "", mimeType: node.content?.mimeType, title: node.name ?? "", path: "", modifiedAt: node.modifiedAt, kind: .file)
     }
 }
