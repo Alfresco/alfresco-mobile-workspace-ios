fbd53792ebe384ff302458a0c737278877acd964
florin.baincescu <florin.baincescu@ness.com>
2021-04-14 15:26:18 +0300

    Added zoom on tap in full screen preview

--- ContentApp/Modules/CameraKit/Screens/FullScreenCapturedAssetViewController.swift
+++ ContentApp/Modules/CameraKit/Screens/FullScreenCapturedAssetViewController.swift
@@ -37,6 +37,11 @@ class FullScreenCapturedAssetViewController: UIViewController {
         imageView.image = imageCapturedAsset
         closeButton.layer.cornerRadius = closeButton.bounds.height / 2.0
         applyComponentsThemes()
+        
+        let tapGesture = UITapGestureRecognizer(target: self,
+                                                action: #selector(zoomImageGesture(_:)))
+        tapGesture.numberOfTapsRequired = 2
+        scrollView.addGestureRecognizer(tapGesture)
     }
     
     // MARK: - IBActions
@@ -44,19 +49,65 @@ class FullScreenCapturedAssetViewController: UIViewController {
     @IBAction func closeButtonTapped(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
+
+    @objc func zoomImageGesture(_ sender: UIGestureRecognizer) {
+        if scrollView.zoomScale >= scrollView.maximumZoomScale / 2.0 {
+            scrollView.setZoomScale(scrollView.minimumZoomScale, animated: true)
+        } else {
+            let center = sender.location(in: sender.view)
+            let zoomRect = zoomRectForScale(3 * scrollView.minimumZoomScale, center: center)
+            scrollView.zoom(to: zoomRect, animated: true)
+        }
+    }
     
     // MARK: - Private Methods
     
     private func applyComponentsThemes() {
         guard let theme = self.theme else { return }
-        
+
+        view.backgroundColor = .black
         closeButton.tintColor = theme.onSurface60Color
-        closeButton.backgroundColor = theme.surfaceColor.withAlphaComponent(0.6)
+        closeButton.backgroundColor = .clear
+    }
+    
+    private func zoomRectForScale(_ scale: CGFloat, center: CGPoint) -> CGRect {
+        var zoomRect = CGRect.zero
+        zoomRect.size.height = imageView.frame.size.height / scale
+        zoomRect.size.width  = imageView.frame.size.width  / scale
+        zoomRect.origin.x    = center.x - (zoomRect.size.width  / 2.0)
+        zoomRect.origin.y    = center.y - (zoomRect.size.height / 2.0)
+        return zoomRect
     }
 }
 
 extension FullScreenCapturedAssetViewController: UIScrollViewDelegate {
+    
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return imageView
+          return imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        if scrollView.zoomScale > 1 {
+            if let image = imageView.image {
+                let ratioW = imageView.frame.width / image.size.width
+                let ratioH = imageView.frame.height / image.size.height
+                let ratio = ratioW < ratioH ? ratioW : ratioH
+                let newWidth = image.size.width * ratio
+                let newHeight = image.size.height * ratio
+                
+                let left = 0.5 * (newWidth * scrollView.zoomScale > imageView.frame.width ? (newWidth - imageView.frame.width) : (scrollView.frame.width - scrollView.contentSize.width))
+
+                let top = 0.5 * (newHeight * scrollView.zoomScale > imageView.frame.height ? (newHeight - imageView.frame.height) : (scrollView.frame.height - scrollView.contentSize.height))
+                
+                scrollView.contentInset = UIEdgeInsets(top: top,
+                                                       left: left,
+                                                       bottom: top,
+                                                       right: left)
+                closeButton.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+            }
+        } else {
+            scrollView.contentInset = UIEdgeInsets.zero
+            closeButton.backgroundColor = .clear
+        }
     }
 }
