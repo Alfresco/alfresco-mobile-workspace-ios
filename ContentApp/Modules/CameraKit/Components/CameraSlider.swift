a95812e7d248df6701c6157f135d81a24eaaf58a
florin.baincescu <florin.baincescu@ness.com>
2021-04-19 10:52:58 +0300

    Fixed CameraSlider background button

--- ContentApp/Modules/CameraKit/Components/CameraSlider.swift
+++ ContentApp/Modules/CameraKit/Components/CameraSlider.swift
@@ -157,7 +157,6 @@ class CameraSliderControl: UIControl {
             entry.sizeToFit()
         }
         for entry in sliderButtonEntries {
-            entry.sizeToFit()
             entry.backgroundColor = sliderStyle.optionBackgroundColor
             entry.layer.cornerRadius = entry.bounds.height / 2.0
             entry.layer.masksToBounds = true
