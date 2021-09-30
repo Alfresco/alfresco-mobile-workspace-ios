d227e5edcca84f4e1fb96d86dd0eff4c3fabc681
ankitgoyalgloballogic <Ankit.Goyal@contractors.onbase.com>
2021-09-30 14:33:38 +0530

    Codacy tool fixes #REF: #MOBILEAPPS-836

--- ContentAppTests/TestSearchViewModel.swift
+++ ContentAppTests/TestSearchViewModel.swift
@@ -21,26 +21,6 @@ import XCTest
 
 class TestSearchViewModel: XCTestCase {
 
-    override func setUp() {
-        // initialize common code here
-    }
-    
-    override func tearDown() {
-        // release objects here
-    }
- 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
     func testSearchViewModel_WhenValidTimeDifferenceProvided_ShouldReturnFalse() {
         let services = CoordinatorServices()
         let searchModel = SearchModel(with: services)
