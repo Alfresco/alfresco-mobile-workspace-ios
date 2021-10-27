3c8cf8dc1d9963854cbf1647adc1d446f5393519
ankitgoyalgloballogic <Ankit.Goyal@contractors.onbase.com>
2021-10-27 13:48:46 +0530

    Number Range Advance Search component #REF: #MOBILEAPPS-907

---
+++ ContentApp/Modules/Advance Search/Number Range/SearchNumberRangeComponentViewController.swift
@@ -0,0 +1,156 @@
+//
+// Copyright (C) 2005-2021 Alfresco Software Limited.
+//
+// This file is part of the Alfresco Content Mobile iOS App.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import UIKit
+import AlfrescoContent
+import MaterialComponents.MaterialTextControls_OutlinedTextFields
+import MaterialComponents.MaterialTextControls_OutlinedTextFieldsTheming
+import MaterialComponents.MaterialButtons
+import MaterialComponents.MaterialButtons_Theming
+
+class SearchNumberRangeComponentViewController: SystemThemableViewController {
+    @IBOutlet weak var baseView: UIView!
+    @IBOutlet weak var headerTitleLabel: UILabel!
+    @IBOutlet weak var dismissButton: UIButton!
+    @IBOutlet weak var divider: UIView!
+    @IBOutlet weak var minRangeTextField: MDCOutlinedTextField!
+    @IBOutlet weak var maxRangeTextField: MDCOutlinedTextField!
+    @IBOutlet weak var horizontalDivider: UIView!
+    @IBOutlet weak var dividerTextField: UIView!
+    @IBOutlet weak var applyButton: MDCButton!
+    @IBOutlet weak var resetButton: MDCButton!
+    lazy var numberRangeViewModel = SearchNumberRangeComponentViewModel()
+    var callback: SearchComponentCallBack?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.layer.cornerRadius = UIConstants.cornerRadiusDialog
+        baseView.layer.cornerRadius = UIConstants.cornerRadiusDialog
+        view.isHidden = true
+        hideKeyboardWhenTappedAround()
+        applyLocalization()
+        applyComponentsThemes()
+        minRangeTextField.becomeFirstResponder()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+        view.isHidden = false
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        calculatePreferredSize(view.bounds.size)
+    }
+    
+    private func calculatePreferredSize(_ size: CGSize) {
+        let targetSize = CGSize(width: size.width,
+                                height: UIView.layoutFittingCompressedSize.height)
+        preferredContentSize = view.systemLayoutSizeFitting(targetSize, withHorizontalFittingPriority: .defaultHigh, verticalFittingPriority: .defaultLow)
+    }
+    
+    // MARK: - Apply Themes and Localization
+    override func applyComponentsThemes() {
+        super.applyComponentsThemes()
+        guard let currentTheme = coordinatorServices?.themingService?.activeTheme,
+              let textFieldScheme = coordinatorServices?.themingService?.containerScheming(for: .loginTextField),
+              let buttonScheme = coordinatorServices?.themingService?.containerScheming(for: .dialogButton),
+              let bigButtonScheme = coordinatorServices?.themingService?.containerScheming(for: .loginBigButton) else { return }
+        
+        view.backgroundColor = currentTheme.surfaceColor
+        headerTitleLabel.applyeStyleHeadline6OnSurface(theme: currentTheme)
+        dismissButton.tintColor = currentTheme.onSurfaceColor
+        divider.backgroundColor = currentTheme.onSurface12Color
+        horizontalDivider.backgroundColor = currentTheme.onSurface12Color
+        dividerTextField.backgroundColor = currentTheme.onSurface12Color
+        
+        minRangeTextField.applyTheme(withScheme: textFieldScheme)
+        minRangeTextField.trailingViewMode = .unlessEditing
+        minRangeTextField.leadingAssistiveLabel.text = ""
+        minRangeTextField.trailingView = nil
+        
+        maxRangeTextField.applyTheme(withScheme: textFieldScheme)
+        maxRangeTextField.trailingViewMode = .unlessEditing
+        maxRangeTextField.leadingAssistiveLabel.text = ""
+        maxRangeTextField.trailingView = nil
+        
+        applyButton.applyContainedTheme(withScheme: buttonScheme)
+        applyButton.isUppercaseTitle = false
+        applyButton.setShadowColor(.clear, for: .normal)
+        applyButton.layer.cornerRadius = UIConstants.cornerRadiusDialog
+
+        resetButton.applyContainedTheme(withScheme: bigButtonScheme)
+        resetButton.setBackgroundColor(currentTheme.onSurface5Color, for: .normal)
+        resetButton.isUppercaseTitle = false
+        resetButton.setShadowColor(.clear, for: .normal)
+        resetButton.setTitleColor(currentTheme.onSurfaceColor, for: .normal)
+        resetButton.layer.cornerRadius = UIConstants.cornerRadiusDialog
+    }
+    
+    private func applyLocalization() {
+        let minValue = numberRangeViewModel.getPrefilledValues().minValue
+        let maxValue = numberRangeViewModel.getPrefilledValues().maxValue
+        headerTitleLabel.text = LocalizationConstants.AdvanceSearch.numberRangeFilter
+        minRangeTextField.label.text = LocalizationConstants.AdvanceSearch.fromKeyword
+        maxRangeTextField.label.text = LocalizationConstants.AdvanceSearch.toKeyword
+        minRangeTextField.text = minValue
+        maxRangeTextField.text = maxValue
+        applyButton.setTitle(LocalizationConstants.AdvanceSearch.apply, for: .normal)
+        resetButton.setTitle(LocalizationConstants.AdvanceSearch.reset, for: .normal)
+    }
+    
+    @IBAction func dismissComponentButtonAction(_ sender: Any) {
+        self.callback?(self.numberRangeViewModel.selectedCategory)
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func applyButtonAction(_ sender: Any) {
+        self.numberRangeViewModel.applyFilter(minValue: minRangeTextField.text, maxValue: maxRangeTextField.text)
+        self.dismissComponentButtonAction(Any.self)
+    }
+    
+    @IBAction func resetButtonAction(_ sender: Any) {
+        self.numberRangeViewModel.resetFilter()
+        self.dismissComponentButtonAction(Any.self)
+    }
+    
+    override func viewWillTransition(to size: CGSize,
+                                     with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        calculatePreferredSize(size)
+    }
+
+    override func willTransition(to newCollection: UITraitCollection,
+                                 with coordinator: UIViewControllerTransitionCoordinator) {
+        super.willTransition(to: newCollection, with: coordinator)
+    }
+}
+
+// MARK: - Text Field Delegate
+extension SearchNumberRangeComponentViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let allowedCharacters = CharacterSet.decimalDigits
+        let characterSet = CharacterSet(charactersIn: string)
+        return allowedCharacters.isSuperset(of: characterSet)
+    }
+}
+
+// MARK: - Storyboard Instantiable
+extension SearchNumberRangeComponentViewController: SearchComponentsStoryboardInstantiable { }
+
---
+++ ContentApp/Modules/Advance Search/Number Range/SearchNumberRangeComponentViewModel.swift
@@ -0,0 +1,69 @@
+//
+// Copyright (C) 2005-2021 Alfresco Software Limited.
+//
+// This file is part of the Alfresco Content Mobile iOS App.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import UIKit
+import AlfrescoContent
+
+class SearchNumberRangeComponentViewModel: NSObject {
+    var selectedCategory: SearchCategories?
+    let stringConcatenator = "-"
+    
+    // MARK: - Update Selected Values
+    func getPrefilledValues() -> (minValue: String?, maxValue: String?) {
+        if let selectedCategory = self.selectedCategory {
+            let component = selectedCategory.component
+            let selectedValue = component?.settings?.selectedValue ?? ""
+            let valuesArray = selectedValue.components(separatedBy: stringConcatenator)
+            if valuesArray.count > 1 {
+                let minimumValue = valuesArray[0].trimmingCharacters(in: .whitespacesAndNewlines)
+                let maximumValue = valuesArray[1].trimmingCharacters(in: .whitespacesAndNewlines)
+                return (minimumValue, maximumValue)
+            }
+        }
+        return (nil, nil)
+    }
+
+    // MARK: - Apply Filter
+    func applyFilter(minValue: String?, maxValue: String?) {
+        let minimumValue = (minValue ?? "").trimmingCharacters(in: .whitespacesAndNewlines)
+        let maximumValue = (maxValue ?? "").trimmingCharacters(in: .whitespacesAndNewlines)
+        if !minimumValue.isEmpty && !maximumValue.isEmpty && Int(maximumValue) ?? 0 > Int(minimumValue) ?? 0 {
+            let value = String(format: "%@ %@ %@", minimumValue, stringConcatenator, maximumValue)
+            if let selectedCategory = self.selectedCategory {
+                let component = selectedCategory.component
+                let settings = component?.settings
+                settings?.selectedValue = value
+                component?.settings = settings
+                selectedCategory.component = component
+                self.selectedCategory = selectedCategory
+            }
+        }
+    }
+    
+    // MARK: - Reset Filter
+    func resetFilter() {
+        if let selectedCategory = self.selectedCategory {
+            let component = selectedCategory.component
+            let settings = component?.settings
+            settings?.selectedValue = nil
+            component?.settings = settings
+            selectedCategory.component = component
+            self.selectedCategory = selectedCategory
+        }
+    }
+}
