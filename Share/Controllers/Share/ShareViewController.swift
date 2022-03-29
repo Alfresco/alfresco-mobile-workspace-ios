6eb6658cf4fa3fa9d7b179e3affd15bc9de2cbca
Ankit Goyal <Ankit.Goyal@contractors.onbase.com>
2022-03-29 23:09:51 +0530

    Fixed google drive issue for file types #REF: #MOBILEAPPS-1120

--- Share/Controllers/Share/ShareViewController.swift
+++ Share/Controllers/Share/ShareViewController.swift
@@ -144,9 +144,22 @@ class ShareViewController: SystemThemableViewController {
                     if let url = data as? URL {
                         urlsArray.append(url)
                         fetchGroup.leave()
-                    } else {
-                        // Handle this situation as you prefer
-                        fatalError("Impossible to save image")
+                    } else if provider.hasItemConformingToTypeIdentifier("public.file-url") {
+                        provider.loadItem(forTypeIdentifier: "public.file-url" as String,
+                                          options: nil) { (data, error) in
+                            // Handle the error here if you want
+                            guard error == nil else { return }
+                            if let url = data as? URL {
+                                urlsArray.append(url)
+                                fetchGroup.leave()
+                            } else {
+        //                         Handle this situation as you prefer
+                                Snackbar.display(with: LocalizationConstants.AppExtension.unsupportedFileFormat,
+                                                 type: .approve,
+                                                 presentationHostViewOverride: self.view,
+                                                 finish: nil)
+                            }
+                        }
                     }
                 }
             }
