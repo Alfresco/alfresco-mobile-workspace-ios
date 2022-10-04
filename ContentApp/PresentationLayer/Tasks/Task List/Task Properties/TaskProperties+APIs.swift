3ecb97c97ad8f7db665ac8bbe124b4f2f7b38aba
Ankit Goyal <Ankit.Goyal@contractors.onbase.com>
2022-10-04 21:56:05 +0530

    added check for internet connectivity

--- ContentApp/PresentationLayer/Tasks/Task List/Task Properties/TaskProperties+APIs.swift
+++ ContentApp/PresentationLayer/Tasks/Task List/Task Properties/TaskProperties+APIs.swift
@@ -26,7 +26,7 @@ extension TaskPropertiesViewModel {
     // MARK: - Task details
 
     func taskDetails(with taskId: String, completionHandler: @escaping (_ taskNodes: TaskNode?, _ error: Error?) -> Void) {
-        
+        guard services?.connectivityService?.hasInternetConnection() == true else { return }
         self.isLoading.value = true
         services?.accountService?.getSessionForCurrentAccount(completionHandler: { authenticationProvider in
             AlfrescoContentAPI.customHeaders = authenticationProvider.authorizationHeader()
@@ -51,6 +51,7 @@ extension TaskPropertiesViewModel {
     // MARK: - Task comment history
 
     func taskComments(with taskId: String, completionHandler: @escaping (_ taskComments: [TaskCommentModel], _ error: Error?) -> Void) {
+        guard services?.connectivityService?.hasInternetConnection() == true else { return }
         self.isLoading.value = true
         services?.accountService?.getSessionForCurrentAccount(completionHandler: { authenticationProvider in
             AlfrescoContentAPI.customHeaders = authenticationProvider.authorizationHeader()
@@ -75,6 +76,7 @@ extension TaskPropertiesViewModel {
     // MARK: - Task add a comment
     
     func addTaskComment(with taskId: String, message: String, completionHandler: @escaping (_ taskComment: [TaskCommentModel], _ error: Error?) -> Void) {
+        guard services?.connectivityService?.hasInternetConnection() == true else { return }
         self.isLoading.value = true
         services?.accountService?.getSessionForCurrentAccount(completionHandler: { authenticationProvider in
             AlfrescoContentAPI.customHeaders = authenticationProvider.authorizationHeader()
@@ -99,6 +101,7 @@ extension TaskPropertiesViewModel {
     // MARK: - Task attachments
 
     func taskAttachments(with taskId: String, completionHandler: @escaping (_ taskAttachments: [TaskAttachmentModel], _ error: Error?) -> Void) {
+        guard services?.connectivityService?.hasInternetConnection() == true else { return }
         self.isLoading.value = true
         services?.accountService?.getSessionForCurrentAccount(completionHandler: { authenticationProvider in
             AlfrescoContentAPI.customHeaders = authenticationProvider.authorizationHeader()
@@ -184,6 +187,7 @@ extension TaskPropertiesViewModel {
     // MARK: - Task attachments
 
     func completeTask(with taskId: String, completionHandler: @escaping (_ isSuccess: Bool) -> Void) {
+        guard services?.connectivityService?.hasInternetConnection() == true else { return }
         self.isLoading.value = true
         services?.accountService?.getSessionForCurrentAccount(completionHandler: { authenticationProvider in
             AlfrescoContentAPI.customHeaders = authenticationProvider.authorizationHeader()
@@ -202,7 +206,7 @@ extension TaskPropertiesViewModel {
     
     // MARK: - Edit Task
     func editTaskDetails(with taskId: String, params: TaskBodyCreate, completionHandler: @escaping ((_ data: TaskNode?, _ error: Error?) -> Void)) {
-        
+        guard services?.connectivityService?.hasInternetConnection() == true else { return }
         self.isLoading.value = true
         services?.accountService?.getSessionForCurrentAccount(completionHandler: { authenticationProvider in
             AlfrescoContentAPI.customHeaders = authenticationProvider.authorizationHeader()
@@ -224,7 +228,7 @@ extension TaskPropertiesViewModel {
     
     // MARK: - Assign Task
     func assignTask(with taskId: String, params: AssignUserBody, completionHandler: @escaping ((_ data: TaskNode?, _ error: Error?) -> Void)) {
-        
+        guard services?.connectivityService?.hasInternetConnection() == true else { return }
         self.isLoading.value = true
         services?.accountService?.getSessionForCurrentAccount(completionHandler: { authenticationProvider in
             AlfrescoContentAPI.customHeaders = authenticationProvider.authorizationHeader()
@@ -269,6 +273,7 @@ extension TaskPropertiesViewModel {
     }
     
     private func deleteAttachment(with attachmentID: Int?, completionHandler: @escaping ((_ success: Bool) -> Void)) {
+        guard services?.connectivityService?.hasInternetConnection() == true else { return }
         if let attachmentID, attachmentID != -1 {
             let contentId = String(format: "%d", attachmentID)
             self.isLoading.value = true
