e842a1598c29d4feec682957fc66cb1e4336d8e8
Ankit Goyal <ankit.goyal@contractors.onbase.com>
2023-08-09 10:15:05 +0530

    added client feedback related to excluded actions for offline tab (#618)
    #REF: #MOBILEAPPS-2049

--- ContentApp/BusinessLayer/Localization/LocalizationConstants.swift
+++ ContentApp/BusinessLayer/Localization/LocalizationConstants.swift
@@ -423,5 +423,6 @@ struct LocalizationConstants {
     
     struct MultipleFilesSelection {
         static let multipleItemsCount = NSLocalizedString("multiple-selected-items-count", comment: "")
+        static let maximumFileSelectionMessage = NSLocalizedString("maximum-items-selection-message", comment: "")
     }
 }
