ce5fada566ef625def99d8d52dff3b926aeeb265
PiplaniVinay <92846583+PiplaniVinay@users.noreply.github.com>
2024-01-15 19:31:59 +0530

    Remove commented code

--- ContentApp/PresentationLayer/Tasks/Task List/Controller/Start Workflow/StartWorkflowViewModel.swift
+++ ContentApp/PresentationLayer/Tasks/Task List/Controller/Start Workflow/StartWorkflowViewModel.swift
@@ -147,7 +147,6 @@ class StartWorkflowViewModel: NSObject {
             
             ProcessAPI.formFields(name: name) {[weak self] data, fields, error in
                 guard let sSelf = self else { return }
-                print(error)
                 sSelf.isLoading.value = false
                 sSelf.isAllowedToEditAssignee = true
                 sSelf.formFields = fields
